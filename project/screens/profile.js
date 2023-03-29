4f8038231fb0c3ff17a66b4c3d4ea2d2e4a8c096
maryam-hussien <maryamhussien122@gmail.com>
2023-03-30 01:41:10 +0200

    profile

--- project/screens/profile.js
+++ project/screens/profile.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View ,Image } from 'react-native';
 import { useState } from 'react'; 
+import profile from '../assets/profile.jpg';
 export default function Profile() {
      return (
       <View style={styles.container}>
         <Text style={styles.title}> My Profile </Text>
+        <view style={styles.buttonContainer}>
         <Button title="My Orders                 >" style={styles.button}/>
-        <Button title="Payment information     >" style={styles.button}/>
+        <Button title="Payment information       >" style={styles.button}/>
         <Button title="Settings                  >" style={styles.button}/>
+        </view>
+        <Image source={profile} style={styles.image} />
 
         <StatusBar style="auto" />
       </View>
@@ -19,24 +23,39 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#fff',
        alignItems: 'center',
-      // justifyContent: 'center',
+       justifyContent: 'space-between',
     },
     title: {
         fontSize: 30,
         color:'black',
         marginBottom: 20,
         position:'relative',
-        alignContent:'flex-start',
+       // alignContent:'flex-start',
+        marginTop:30,
     
       },
-      button: {
-        backgroundColor: '#eee',
-        padding: 10,
-        borderRadius: 4,
-        alignItems:'flex-start',
-        width:'fitContent',
-        marginBottom:10,
+      buttonContainer:{
+        marginBottom:350,
+       justifyContent:'space-between',
+        width:'80%',
+
       },
+      button: {
+    backgroundColor: 'rgba(46, 46, 57, 0.7)',
+    justifyContent:'space-between',
+    padding: 10,
+    paddingLeft:0,
+    borderRadius: 4,
+    alignItems:'center',
+    width:'30%',
+    marginBottom:10,
+  },
+  image:{
+    width:100,
+    height:100,
+    top:100,
+    position:'absolute',
+  }
 
       
 });
