83308a9e28e6414133152c37dc43ae369f013761
maryam-hussien <maryamhussien122@gmail.com>
2023-03-31 16:34:28 +0200

    profile

--- project/screens/profile.js
+++ project/screens/profile.js
@@ -1,17 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View ,Image } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View ,Image,TouchableOpacity } from 'react-native';
 import { useState } from 'react'; 
 import profile from '../assets/profile.jpg';
-export default function Profile() {
+import auth from '../firebase/firebase';
+export default function Profile({navigation}) {
+    const [email, setEmail] =useState('');
+    const [password, setPassword] = useState('');
+    const handleSignOut = () => {
+        navigation.navigate('Home');
+        signOut (auth)
+          .then(() => {
+            setIsSignedIn(false);
+            setMsg('signOut Done');
+          })
+        .catch((error) => {
+          seterrorMsg(error.message);
+    });
+}
      return (
       <View style={styles.container}>
+
         <Text style={styles.title}> My Profile </Text>
-        <view style={styles.buttonContainer}>
-        <Button title="My Orders                 >" style={styles.button}/>
-        <Button title="Payment information       >" style={styles.button}/>
-        <Button title="Settings                  >" style={styles.button}/>
-        </view>
+
+        <View style={styles.buttonContainer}>
+            <TextInput
+           style={styles.input}
+           placeholder="User Name"
+           value='Maryoma'
+         />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={setEmail}
+          value={email}
+        />
+          <TextInput
+         style={styles.input}
+         placeholder="Mobile Number"
+         value={"01100297301"}
+       />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          secureTextEntry={true}
+          onChangeText={setPassword}
+        />
+
+        </View>
+
         <Image source={profile} style={styles.image} />
+        <TouchableOpacity style={styles.SignOutbutton}  onPress={handleSignOut}>
+        <Text style={styles.SignOutbuttontext}>Sign Out </Text>
+        </TouchableOpacity>
 
         <StatusBar style="auto" />
       </View>
@@ -23,39 +64,51 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#fff',
        alignItems: 'center',
-       justifyContent: 'space-between',
+     
     },
     title: {
         fontSize: 30,
         color:'black',
         marginBottom: 20,
         position:'relative',
-       // alignContent:'flex-start',
         marginTop:30,
     
       },
+   
       buttonContainer:{
-        marginBottom:350,
-       justifyContent:'space-between',
-        width:'80%',
+            flexDirection:'coulmn',
+            justifyContent:'space-between',
+            width:'80%',
+            marginTop:130,
+            
+          },
 
-      },
-      button: {
-    backgroundColor: 'rgba(46, 46, 57, 0.7)',
-    justifyContent:'space-between',
-    padding: 10,
-    paddingLeft:0,
-    borderRadius: 4,
-    alignItems:'center',
-    width:'30%',
-    marginBottom:10,
-  },
-  image:{
-    width:100,
-    height:100,
-    top:100,
-    position:'absolute',
-  }
+     image:{
+          width:100,
+          height:100,
+          top:100,
+          position:'absolute',
+        },
+        SignOutbuttontext:{
+            color:'#eee',
+          },
+        SignOutbutton:{
+            backgroundColor: 'black',
+            padding: 10,
+            borderRadius: 10,
+            width:'30%',
+            alignItems:'center',
+            marginTop:90,
+        },
+        input: {
+            borderWidth: 2,
+            borderColor: 'black',
+            borderRadius: 8,
+            padding: 10,
+            width: '83%',
+            marginBottom: 20,
+           
+          },
 
       
 });
