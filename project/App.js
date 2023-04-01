0a90d7c503c073a4ed3f198f83d0fdd3291a1d76
mariemmohamed374 <marimmemo849@gmail.com>
2023-04-02 00:58:57 +0200

    I completed the pages

--- project/App.js
+++ project/App.js
@@ -4,23 +4,37 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInPage from './screens/signin';
 import HomeScreen from './screens/homescreen';
 import SignupPage from './screens/signup';
-
+import Welcome from './screens/Welcome';
+import Start from './screens/Start';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Welcome">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ headerShown: false }}
         />
+         <Stack.Screen
+          name="Welcome1"
+          component={Welcome}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen
+          name="Start1"
+          component={Start}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="SignInPage" component={SignInPage} />
+        <Stack.Screen name="Welcome" component={Welcome} />
         <Stack.Screen name="SignupPage" component={SignupPage} />
-        <Stack.Screen name="Welcome" component={Welcome} />
-        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Start" component={Start} />
+
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
--- project/screens/HomeScreen.js
+++ project/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, View, Text,StyleSheet,TextInput } from 'react-native';
+import { Button, View, Text,StyleSheet, } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   return (
