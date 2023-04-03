42ac9ece7ff9124d519f6f10c098a5e98ab37296
mohamed-wageh <80712072+mohamed-wageh@users.noreply.github.com>
2023-04-03 21:28:35 +0200

    fix error

--- project/navigation/index.js
+++ project/navigation/index.js
@@ -15,7 +15,7 @@ const index = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Profile"
+        initialRouteName="Welcome"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Start" component={Start} />
